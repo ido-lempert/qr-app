d6e4a3de4e4502a49c2e4b0f18c7d7092cc0b1c6
ido-lempert <il.mrbit@gmail.com>
2023-02-06 13:55:37 +0200

    add icons and screenshots

---
+++ src/app/pwa.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {fromEvent, ReplaySubject, map, Subject, pipe, tap, take, mergeMap, Observable} from "rxjs";
+import {fromPromise} from "rxjs/internal/observable/innerFrom";
+
+interface BeforeInstallPrompt extends Event{
+  prompt : () => Promise<any>;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PwaService {
+  private beforeInstallPrompt = new ReplaySubject<BeforeInstallPrompt>(1);
+
+  constructor() {
+
+  }
+
+  init(){
+    fromEvent<BeforeInstallPrompt>(window, 'beforeinstallprompt')
+        .pipe(take(1))
+        .subscribe(beforeInstallPrompt => {
+          beforeInstallPrompt.preventDefault();
+          this.beforeInstallPrompt.next(beforeInstallPrompt);
+        });
+  }
+
+  get prompt(): Observable<any> {
+    return this.beforeInstallPrompt
+        .pipe(
+            mergeMap(
+                (installer) => fromPromise(installer.prompt())
+            )
+        );
+  }
+}
