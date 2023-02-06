b996ec0575ad351c28bd3d58eea83acacc7f0954
ido-lempert <il.mrbit@gmail.com>
2023-02-06 13:48:36 +0200

    add icons and screenshots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import * as QRCode from 'qrcode'
+import {PwaService} from "./pwa.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'qr-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   qrUrl?: string;
+  private pwaServiceSubscriber?: Subscription;
+
+  constructor(private pwaService: PwaService) {
+  }
+
   async generate(el: HTMLTextAreaElement){
     const url = await QRCode.toDataURL(el.value, {width: 600});
     this.qrUrl = url;
     el.value = '';
+
+    this.pwaServiceSubscriber = this.pwaService.prompt.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.pwaServiceSubscriber?.unsubscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import {PwaService} from "./pwa.service";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +13,10 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   // or after 30 seconds (whichever comes first).
   registrationStrategy: 'registerWhenStable:30000'
 })],
-  providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private pwaService: PwaService) {
+    pwaService.init();
+  }
+}
